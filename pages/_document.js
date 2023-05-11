7f39e987150b4bad57c5af923d715488046cbbed
Lattana <lattana@hellosezame.com>
2023-05-11 15:48:40 +0200

    [ADD] width fit

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html className="w-fit">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
